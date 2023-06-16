1c6bcb93da9e4fd20e231b16933d83db446aaf64
Apah <junerob4shot@gmail.com>
2023-06-16 12:48:47 +0100

    first program that prints a random number and checks if it is +, - or 0

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <time.h>
+/**
+  * main - Entry point
+  *
+  * Description: This function prints out a generated
+  *		random number
+  * Return: Always 0 (Success)
+  */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	if (n < 0)
+	{
+		printf("%d is negative", n);
+	}
+	else if (n > 0)
+	{
+		printf("%d is positive", n);
+	}
+	else if (n == 0)
+	{
+		printf("%d is zero", n);
+	}
+	return (0);
+}
