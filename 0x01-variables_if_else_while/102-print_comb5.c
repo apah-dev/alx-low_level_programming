282b1889d0aed65396bfd0174e9c4d9144f4cea2
Apah <junerob4shot@gmail.com>
2023-06-16 21:31:00 +0100

    program to print all possible combo of two digits

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+  * main - Entry point
+  *
+  * Description: This program uses a while loop to generate
+  *		lowercase alphabets
+  * Return: Always 0 (Success)
+  */
+int main(void)
+{
+	int first_digit;
+	int second_digit;
+
+	for (first_digit = 0; numbers <= 99; first_digit++)
+	{
+		printf("%d", numbers);
+
+		if (numbers < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
