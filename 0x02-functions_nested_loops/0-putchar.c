5e15143531a0d5b5749416392ab1fbc75d97f6ba
Apah <junerob4shot@gmail.com>
2023-06-20 10:44:12 -0700

    putchar program and function created

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,13 @@
-#include <stdio.h>
+#include "main.h"
 /**
   * main - Entry point
+  * Description - prints putchar using created header file
   *
   * Return: Always 0 (Success)
   */
 int main(void)
 {
-        printf("_putchar\n");
-        return (0);
+	_putchar("_putchar");
+
+	return (0);
 }
