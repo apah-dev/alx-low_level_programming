558421d6bb351668e89559f698f19ae04d7ba892
Apah <junerob4shot@gmail.com>
2023-06-20 12:13:03 -0700

    function that prnts lower case letters 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Prints all alphabets in lowercase letters 10 times
+ *
+ * Description: This function prints all alphabets in lowercase 10 times
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+	int lowercase;
+	int i;
+
+	while (i <= 10)
+	{
+		for (lowercase = 97; lowercase <= 122; lowercase++)
+		{
+			_putchar(lowercase);
+		}
+	}
+	i++;
+	_putchar('\n');
+}
