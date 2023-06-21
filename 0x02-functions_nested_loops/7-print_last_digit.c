4f1994eb27fd3a0d9171755cb8d352336ced9fc7
Apah Benson <junerob4shot@gmail.com>
2023-06-21 02:27:05 +0100

    one more commit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,12 +8,13 @@
  */
 int print_last_digit(int n)
 {
+	
 	int last_value = n % 10;
 	if (last_value < 0)
 		last_value = -last_value;
 
 	_putchar('0' + last_value);
 
-	return last_value;
+	return (last_value);
 }
 
