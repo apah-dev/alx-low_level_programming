a45d5825836dd81f1a52ae4f313b0a72bf464662
Apah Benson <junerob4shot@gmail.com>
2023-06-21 03:36:23 +0100

    times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,7 +8,8 @@
 void times_table(void)
 {
 	int i, j, result;
-	
+	int count = 0;
+
 	for (i = 0; i <= 9; i++)
 	{
 		for (j = 0; j <= 9; j++)
@@ -17,27 +18,35 @@ void times_table(void)
 			if (j == 0)
 			{
 				_putchar('0');
+				count++;
 			}
 			else if (result < 10)
 			{
 				_putchar(' ');
 				_putchar(' ');
 				_putchar(result + '0');
+				count += 3;
 			}
 			else
 			{
 				_putchar(' ');
 				_putchar((result / 10) + '0');
 				_putchar((result % 10) + '0');
+				count += 3;
 			}
 			if (j != 9)
 			{
 				_putchar(',');
 				_putchar(' ');
+				count += 2;
 			}
 		}
 
 		_putchar('\n');
+		count ++;
+
+		if (count >= 380)
+			break;
 	}
 }
 
