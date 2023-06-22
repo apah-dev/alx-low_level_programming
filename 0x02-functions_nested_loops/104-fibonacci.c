cb62c0132ff81d0b6b5b6d05e60d3a5203fdbe60
Apah Benson <junerob4shot@gmail.com>
2023-06-22 13:08:10 +0100

    last fibonacci program

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the first 98 Fibonacci numbers.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int count;
+	unsigned long fib1 = 1, fib2 = 2, fib_next;
+
+	printf("%lu, %lu", fib1, fib2);
+
+	for (count = 3; count <= 98; count++)
+	{
+		fib_next = fib1 + fib2;
+
+		printf(", %lu", fib_next);
+
+		fib1 = fib2;
+		fib2 = fib_next;
+	}
+
+	printf("\n");
+	return 0;
+}
+
