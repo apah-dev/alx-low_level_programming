7183c108f0dadf53fe28e8975c31bbb34ab8664c
Apah Benson <junerob4shot@gmail.com>
2023-06-22 16:57:23 +0100

    program that multiplies two numbers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isdigit - Checks if a character is a digit (0 through 9).
+ * @c: The character to be checked.
+ *
+ * Return: 1 if @c is a digit, 0 otherwise.
+ */
+int mul(int a, int b)
+{
+
+	int result;
+
+	result = a * b;
+
+	return (result);
+}
+
