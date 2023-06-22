52a02dc9d25f14c205be762655a97647d0d311da
Apah <junerob4shot@gmail.com>
2023-06-22 02:28:56 -0700

    another trial for the add two integers program

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,12 +2,14 @@
 
 /**
  * add - adds two integers and returns the result
- *
+ * @a: first int value
+ * @b: second int value
  * Return: result
  */
 int add(int a, int b)
 {
-	int a, b;
+	int a;
+	int b;
 	int result;
 
 	result = a + b;
