8f48ce7f0e99a38d969d09d5c04e337f2274cd32
Apah Benson <junerob4shot@gmail.com>
2023-06-23 00:44:15 +0100

    another commit for print_triangle

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -8,19 +8,27 @@ void print_triangle(int size)
 {
 	int i, j;
 
-	if (size <= 0)
+	if (size > 0)
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < size; j++)
+			{
+				if (j < size - (i + 1)
+				{
+					_putchar(' ');
+				}
+				else
+				{
+					_putchar('#');
+				}
+			}
+			_putchar('\n');
+		}
+	}
+	else
 	{
 		_putchar('\n');
-		return;
-	}
-
-	for (i = 0; i < size; i++)
-	{
-		for (j = 0; j <= i; j++)
-		{
-			_putchar('#');
-		}
-	_putchar('\n');
 	}
 }
 
