8d06bea84054754985eff9636c1761aea9b5df17
Apah Benson <junerob4shot@gmail.com>
2023-06-22 12:01:05 +0100

    another trial for fibonacci

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,10 +1,5 @@
 #include <stdio.h>
 
-int _putchar(char c)
-{
-return putchar(c);
-}
-
 /**
 * main - Prints the first 50 Fibonacci numbers.
 *
@@ -13,33 +8,20 @@ return putchar(c);
 int main(void)
 {
 	int i;
-	unsigned long int fib1 = 1, fib2 = 2, fib_next;
+	unsigned long fib1 = 1, fib2 = 2, fib_next;
 
-	_putchar(fib1 + '0');
-	_putchar(',');
-	_putchar(' ');
-	_putchar(fib2 + '0');
+	printf("%lu, %lu", fib1, fib2);
 
 	for (i = 3; i <= 50; i++)
 	{
 		fib_next = fib1 + fib2;
-		_putchar(',');
-		_putchar(' ');
-		if (fib_next >= 10)
-		{
-			_putchar((fib_next / 10) + '0');
-			_putchar((fib_next % 10) + '0');
-		}
-		else
-		{
-			_putchar(fib_next + '0');
-		}
+		printf(", %lu", fib_next);
 
 		fib1 = fib2;
 		fib2 = fib_next;
 	}
 
-	_putchar('\n');
+	printf("\n");
 	return (0);
 }
 
