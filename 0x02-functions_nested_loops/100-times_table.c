58b55e239f879902975a401e497daa22fdd15f4a
Apah Benson <junerob4shot@gmail.com>
2023-06-22 10:50:08 +0100

    trial for 100 times table

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* print_times_table - prints the n times table starting from 0
+* @n: number of times
+*
+* Description: This function prints the multiplication table for the given
+* value of n. The table starts from 0 and goes up to n times.
+* If n is greater than 15 or less than 0, nothing is printed.
+*/
+void print_times_table(int n)
+{
+	if (n <= 15 && n >= 0)
+	{
+		int row, column, product;
+
+		for (row = 0; row <= n; row++)
+		{
+			for (column = 0; column <= n; column++)
+			{
+				product = row * column;
+				printf("%d", product);
+
+				if (column != n)
+					printf(", ");
+			}
+
+			printf("\n");
+		}
+	}
+}
+
