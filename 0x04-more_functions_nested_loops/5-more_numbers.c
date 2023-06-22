b9aed9df8e50003537e43f67b107edd2f8d824d0
Apah Benson <junerob4shot@gmail.com>
2023-06-22 19:55:11 +0100

    program to print numbers 0-14 ten times

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * more_numbers - Prints the numbers 0 - 14 ten times.
+ *
+ * Return: 0 (Always successful)
+ */
+
+void print_numbers(void)
+{
+	int i;
+	int j;
+	int digit;
+
+	for (i = 0; i <= 10; i++)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+			digit = j;
+
+			if (digit > 9)
+			{
+				_putchar('1');
+				digit %= 10;
+			}
+
+			_putchar('0' + digit);
+		}
+	_putchar('\n');
+	}
+}
