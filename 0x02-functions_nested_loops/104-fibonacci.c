f97c1a41343ebefbca91c21a49e35729a15b149e
Apah Benson <junerob4shot@gmail.com>
2023-06-22 13:52:21 +0100

    another trial

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -8,20 +8,21 @@
 int main(void)
 {
 	int count;
-	unsigned long fib1 = 1, fib2 = 2, fib_next;
+	unsigned int fib1 = 1, fib2 = 2, fib_next;
 
-	printf("%lu, %lu", fib1, fib2);
+	printf("%u, %u", fib1, fib2);
 
 	for (count = 3; count <= 98; count++)
 	{
 		fib_next = fib1 + fib2;
 
-		printf(", %lu", fib_next);
+		printf(", %u", fib_next);
 
 		fib1 = fib2;
 		fib2 = fib_next;
 	}
 
+	printf("\n");
 	return (0);
 }
 
