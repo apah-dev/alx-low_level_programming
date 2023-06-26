ac88fcbb8a5f7634e0ca56a1825ac3e4b2f0c4fa
Apah Benson <junerob4shot@gmail.com>
2023-06-26 22:52:45 +0100

    another commit

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string to stdout in reverse
+ * @s: pointer character that holds the string
+**/
+
+void print_rev(char *s)
+{
+	if (*s == '\0')
+		return;
+
+	_print_rev(s +1);
+	_putchar(*s);
+}
