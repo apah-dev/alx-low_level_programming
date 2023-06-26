7998ef8dd70ead4765286f7b7a434136e78f5276
Apah Benson <junerob4shot@gmail.com>
2023-06-26 22:15:07 +0100

    program that prints the length of the string created

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string passed
+ * @n: pointer that stores the string
+ * Return: length of the char
+**/
+
+int _strlen(char *s)
+{
+	int len;
+	len = strlen(*s);
+	return(len);
+}
