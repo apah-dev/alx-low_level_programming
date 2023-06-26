c3c29c546184eb8318b6d8e9f6355a4106924338
Apah Benson <junerob4shot@gmail.com>
2023-06-26 22:20:00 +0100

    another commit for the length of string program

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -3,12 +3,17 @@
 /**
  * _strlen - returns the length of a string passed
  * @n: pointer that stores the string
+ * @len: iterates through the string
  * Return: length of the char
 **/
 
 int _strlen(char *s)
 {
-	int len;
-	len = strlen(*s);
+	int len = 0;
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
 	return(len);
 }
