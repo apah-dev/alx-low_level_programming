8b24a1ca1b8a50f6b406b3117cf224917a6190ba
Apah Benson <junerob4shot@gmail.com>
2023-06-27 00:35:58 +0100

    puts half program created

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+* puts_half - prints half of a string
+* @str: pointer to the string
+**/
+void puts_half(char *str)
+{
+	int length = strlen(str);
+	int start_index;
+	int i;
+
+	if (length % 2 == 0)
+	{
+		start_index = length / 2;
+	}
+	else
+	{
+		start_index = (length - 1) / 2;
+	}
+
+	for (i = start_index; i < length; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
+
