56784c072988668ccfde6be9d72a8e38fc46cfc7
Apah Benson <junerob4shot@gmail.com>
2023-06-26 22:05:51 +0100

    amendment made to the swap program

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -4,7 +4,6 @@
  * swap_int - swaps the value of two integers a and b.
  * @a: pointer datatype that stores the value of a
  * @b: pointer datatype that stores the value of b
- * @temp: stores the value of a temporarily
 **/
 
 void swap_int(int *a, int *b)
