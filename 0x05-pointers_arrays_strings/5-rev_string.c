ee00381cfaabb4d9086a25f8f7f7a680d7c4fdba
Apah Benson <junerob4shot@gmail.com>
2023-06-27 00:17:20 +0100

    another commit after removing spaces in the code

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -3,22 +3,22 @@
 #include "main.h"
 
 /**
- * rev_string - reverses a string
- * @s: pointer to the string
- **/
+* rev_string - reverses a string
+* @s: pointer to the string
+**/
 void rev_string(char *s)
 {
-    int length = strlen(s);
-    int start = 0;
-    int end = length - 1;
+	int length = strlen(s);
+	int start = 0;
+	int end = length - 1;
 
-    while (start < end)
-    {
-        char temp = s[start];
-        s[start] = s[end];
-        s[end] = temp;
-        start++;
-        end--;
-    }
+	while (start < end)
+	{
+		char temp = s[start];
+		s[start] = s[end];
+		s[end] = temp;
+		start++;
+		end--;
+	}
 }
 
