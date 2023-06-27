df0fabe7e93c0b8ad82df7c1da0eb371b240c761
Apah Benson <junerob4shot@gmail.com>
2023-06-27 01:36:03 +0100

    another commit

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -4,19 +4,18 @@
 #include <math.h>
 
 /**
-* main - generate a random password.
-*
-* Return: 0
-*/
+ * main - print password.
+ *
+ * Return: 0.
+ */
+
 int main(void)
 {
-	int total = 2772;
-	int count = 0;
-	int index, random;
+	int total = 2772, count = 0, index, random;
 	char password[100];
 	time_t t;
 
-	srand((int)time(&t));
+	srand((int) time(&t));
 	while (total > 126)
 	{
 		random = rand() % 126;
@@ -24,23 +23,17 @@ int main(void)
 		total -= random;
 		count++;
 	}
-
 	if (total > 0)
-	{
 		password[count] = total;
-		count++;
-	}
 	else
 	{
 		count--;
 	}
-
+	
 	for (index = 0; index <= count; index++)
 	{
 		printf("%c", password[index]);
 	}
-	printf("\n");
-
 	return (0);
 }
 
