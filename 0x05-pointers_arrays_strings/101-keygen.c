c7e56b036d63c00f1ccda2427047f4b5e8a0d1ad
Apah Benson <junerob4shot@gmail.com>
2023-06-27 01:09:05 +0100

    another commit

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,32 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <string.h>
 
 /**
-* generate_password - generates a random valid password
-* Return: the generated password
+* generate_password - generates a random password
+* @length: length of the password
+* Return: pointer to the generated password
 **/
-char *generate_password()
+char *generate_password(int length)
 {
-	static char password[7];
+	char *password = malloc((length + 1) * sizeof(char));
 	int i;
 
 	srand(time(NULL));
 
-	for (i = 0; i < 6; i++)
+	for (i = 0; i < length; i++)
 	{
-		password[i] = rand() % 26 + 'A';
+		password[i] = rand() % 94 + 33;
 	}
-	password[6] = '\0';
+
+	password[length] = '\0';
 
 	return (password);
 }
 
 int main(void)
 {
-	char *password = generate_password();
+	char *password = generate_password(10);
 
-	printf("Generated Password: %s\n", password);
+	printf("Password: %s\n", password);
+
+	free(password);
 
 	return (0);
 }
