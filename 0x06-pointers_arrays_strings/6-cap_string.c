9022a20b6747ea4205bae8c946d2c27cd9422322
Apah Benson <junerob4shot@gmail.com>
2023-06-29 12:12:33 +0100

    another commit

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -2,38 +2,38 @@
 #include <stdbool.h>
 
 /**
- * cap_string - capitalizes all words of a string
- * @str: the string to modify
- *
- * Return: pointer to the modified string
- */
+* cap_string - capitalizes all words of a string
+* @str: the string to modify
+*
+* Return: pointer to the modified string
+*/
 char *cap_string(char *str)
 {
-    int i = 0;
-    bool capitalize = true;
+	int i = 0;
+	bool capitalize = true;
 
-    while (str[i] != '\0')
-    {
-        if (str[i] == ' ' || str[i] == '\t' || str[i] == '\n' ||
-            str[i] == ',' || str[i] == ';' || str[i] == '.' ||
-            str[i] == '!' || str[i] == '?' || str[i] == '"' ||
-            str[i] == '(' || str[i] == ')' || str[i] == '{' || str[i] == '}')
-        {
-            capitalize = true;
-        }
-        else if (capitalize && (str[i] >= 'a' && str[i] <= 'z'))
-        {
-            str[i] = str[i] - 'a' + 'A';
-            capitalize = false;
-        }
-        else
-        {
-            capitalize = false;
-        }
+	while (str[i] != '\0')
+	{
+		if (str[i] == ' ' || str[i] == '\t' || str[i] == '\n' ||
+		str[i] == ',' || str[i] == ';' || str[i] == '.' ||
+		str[i] == '!' || str[i] == '?' || str[i] == '"' ||
+		str[i] == '(' || str[i] == ')' || str[i] == '{' || str[i] == '}')
+		{
+			capitalize = true;
+		}
+		else if (capitalize && (str[i] >= 'a' && str[i] <= 'z'))
+		{
+			str[i] = str[i] - 'a' + 'A';
+			capitalize = false;
+		}
+		else
+		{
+			capitalize = false;
+		}
 
-        i++;
-    }
+		i++;
+	}
 
-    return (str);
+	return (str);
 }
 
