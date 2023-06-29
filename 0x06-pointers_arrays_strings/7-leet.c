6fdcd9b953a6c385ac2e7bb290b173156f789562
Apah Benson <junerob4shot@gmail.com>
2023-06-29 12:14:37 +0100

    another leet commit

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,29 +1,29 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
- * leet - encodes a string into 1337
- * @str: the string to encode
- *
- * Return: pointer to the encoded string
- */
+* leet - encodes a string into 1337
+* @str: the string to encode
+*
+* Return: pointer to the encoded string
+*/
 char *leet(char *str)
 {
-    char *leet_chars = "aAeEoOtTlL";
-    char *leet_replacements = "4433007711";
-    int i, j;
+	char *leet_chars = "aAeEoOtTlL";
+	char *leet_replacements = "4433007711";
+	int i, j;
 
-    for (i = 0; str[i] != '\0'; i++)
-    {
-        for (j = 0; leet_chars[j] != '\0'; j++)
-        {
-            if (str[i] == leet_chars[j])
-            {
-                str[i] = leet_replacements[j];
-                break;
-            }
-        }
-    }
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (j = 0; leet_chars[j] != '\0'; j++)
+		{
+			if (str[i] == leet_chars[j])
+			{
+				str[i] = leet_replacements[j];
+				break;
+			}
+		}
+	}
 
-    return str;
+	return (str);
 }
 
