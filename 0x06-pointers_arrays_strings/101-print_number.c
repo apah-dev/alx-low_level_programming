9ed6742c35465b9b9863d75281e404a3af1e8a0e
Apah Benson <junerob4shot@gmail.com>
2023-06-29 22:29:58 +0100

    another commit

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -6,17 +6,14 @@
 */
 void print_number(int n)
 {
-	unsigned int n1;
-
-	n1 = n;
 	if (n < 0)
 	{
 		_putchar('-');
-		n1 = -n;
+		n = -n;
 	}
-	if (n1 / 10 != 0)
+	if (n / 10)
 	{
 		print_number(n / 10);
 	}
-	_putchar((n1 % 10) + '0');
+	_putchar('0' + n % 10);
 }
