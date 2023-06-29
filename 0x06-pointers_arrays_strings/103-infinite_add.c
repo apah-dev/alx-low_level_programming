8afdcfe584503b3faf72c5ca15417d6850c44c39
Apah Benson <junerob4shot@gmail.com>
2023-06-29 22:43:09 +0100

    another commit

---
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * infinite_add - Adds two numbers.
+ * @n1: The first number as a string.
+ * @n2: The second number as a string.
+ * @r: The buffer to store the result.
+ * @size_r: The size of the buffer.
+ *
+ * Return: Pointer to the result (r), or 0 if result cannot be stored.
+ */
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+	int carry = 0;
+	int i = 0;
+	int digit1, digit2, sum;
+
+	while (n1[i] != '\0' || n2[i] != '\0')
+	{
+		digit1 = (n1[i] != '\0') ? (n1[i] - '0') : 0;
+		digit2 = (n2[i] != '\0') ? (n2[i] - '0') : 0;
+
+		sum = digit1 + digit2 + carry;
+		carry = sum / 10;
+
+		if (i >= size_r - 1 || carry > 0)
+			return (0);
+
+		r[i] = sum % 10 + '0';
+		i++;
+	}
+
+	r[i] = '\0';
+
+	return (r);
+}
+
