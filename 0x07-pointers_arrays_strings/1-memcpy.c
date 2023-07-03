c48204ee9168fa687aeb6faf9556b6cfea942d36
Apah Benson <junerob4shot@gmail.com>
2023-07-03 18:06:45 +0100

    another commit

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -9,11 +9,12 @@
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
+	char *ptr = dest;
 	unsigned int i;
 	
 	for (i = 0; i < n; i++)
 	{
-		dest[i] = src[i];
+		*ptr++ = *src++;
 	}
 	
 	return (dest);
