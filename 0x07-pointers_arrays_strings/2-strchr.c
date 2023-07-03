c6ecb1ed8bfb7d646ebea968b586340cc4a13544
Apah Benson <junerob4shot@gmail.com>
2023-07-03 20:28:03 +0100

    another commit

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -10,13 +10,14 @@
  */
 char *_strchr(char *s, char c)
 {
-	while (*s != '\0')
+	int i = 0;
+
+	for (; s[i] != '\0'; i++)
 	{
-		if (*s == c)
+		if (s[i] == c)
 		{
-			return (s);
+			return (&s[i]);
 		}
-		s++;
 	}
 	return (NULL);
 }
