31ddc4207a874edfe4ebcbe0615c584e2fc0c713
Apah Benson <junerob4shot@gmail.com>
2023-07-03 20:43:46 +0100

    another commit

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -5,7 +5,7 @@
  * @s: pointer to the string to search
  * @accept: pointer to the string containing the characters to accept
  *
- * Return: The number of bytes in the initial segment of s 
+ * Return: The number of bytes in the initial segment of s
  *	consisting only of bytes from accept
  */
 unsigned int _strspn(char *s, char *accept)
