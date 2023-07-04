4bb02dda88d68a2792a67a4a6928603d578b108a
Apah Benson <junerob4shot@gmail.com>
2023-07-04 23:35:36 +0100

    another commit

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdio.h>
+#include <string.h>
 /**
  * _strchr - locates a character in a string
  * @s: pointer to the string to search
@@ -10,13 +11,5 @@
  */
 char *_strchr(char *s, char c)
 {
-	while (*s != '\0')
-	{
-		if (*s == c)
-		{
-			return (s);
-		}
-		s++;
-	}
-	return (0);
+	return (strchr(s, c));
 }
